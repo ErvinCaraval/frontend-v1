33bf8df458735f701a8c34d0dae440d29b70b2fe
ErvinCaraval <ervin.caravali@correounivalle.edu.co>
2025-09-20 11:48:10 -0500

    Limpiar repo: solo frontend

--- backend/routes/games.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const gamesController = require('../controllers/gamesController');
-
-router.get('/', gamesController.listPublicGames);
-
-module.exports = router;
